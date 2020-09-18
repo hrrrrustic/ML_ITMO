0ca633fff586d4bf69a5e3b8741759df20d1ac21
Vlad <hrrrrustic@gmail.com>
2020-09-18 03:31:28 +0300

    taskc progress

--- MLCodeForces/DoubleExtensions.cs
+++ MLCodeForces/DoubleExtensions.cs
@@ -2,8 +2,5 @@
 
 namespace MLCodeForces
 {
-    public static class DoubleExtensions
-    {
-        public static Double SafeValue(this Double value) => Double.IsNaN(value) ? 0 : value;
-    }
+    
 }
--- MLCodeForces/ObjectDescription.cs
+++ MLCodeForces/ObjectDescription.cs
@@ -1,17 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace MLCodeForces
 {
-    public class ObjectDescription
-    {
-        private readonly Int32[] _coordinates;
-
-        private readonly Int32 _value;
-
-        public ObjectDescription(Int32[] coordinates, Int32 value)
-        {
-            _coordinates = coordinates;
-            _value = value;
-        }
-    }
-}
+   
+}   
