4d4f198937915733e953f5a2a06989ed61a997cc
Vlad <hrrrrustic@gmail.com>
2020-09-19 01:39:29 +0300

    init lab1

---
+++ NonparametricRegression/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+using Microsoft.ML;
+
+namespace NonparametricRegression
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MLContext context = new MLContext();
+            IDataView data = context.Data.LoadFromTextFile<SeedData>("D:\\VSProjects\\ITMO\\MLCodeForces\\Lab1DataSet.csv", ',', true);
+            context.Data.CreateEnumerable<SeedData>(data, false).ToList().ForEach(Console.WriteLine);
+        }
+    }
+}
---
+++ NonparametricRegression/SeedData.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.ML.Data;
+
+namespace NonparametricRegression
+{
+    public class SeedData
+    {
+        [LoadColumn(0)] public Single V1 { get; set; }
+        [LoadColumn(1)] public Single V2 { get; set; }
+        [LoadColumn(2)] public Single V3 { get; set; }
+        [LoadColumn(3)] public Single V4 { get; set; }
+        [LoadColumn(4)] public Single V5 { get; set; }
+        [LoadColumn(5)] public Single V6 { get; set; }
+        [LoadColumn(6)] public Single V7 { get; set; }
+        [LoadColumn(7)] public Int32 Class { get; set; }
+
+        public override String ToString() => $"{V1} {V2} {V3} {V4} {V5} {V6} {V7} {Class}";
+    }
+}
